bf8c43f656b4f2e5b0070999c04fb20f74272b04
tragewegen <59048676+tragewegen@users.noreply.github.com>
2023-07-03 15:38:04 +0200

    Update baseLayers.js

--- src/baseLayers.js
+++ src/baseLayers.js
@@ -81,7 +81,7 @@ let nginow = new XYZ({
 
 let abw = new WMTS({
   //info: https://tile.informatievlaanderen.be/ws/raadpleegdiensten/wmts?request=getcapabilities&service=wmts&version=1.0.0 
-    url: 'https://tile.informatievlaanderen.be/ws/raadpleegdiensten/wmts',
+    url: 'https://geo.api.vlaanderen.be/histcart/wmts',
     attributions: ["Vlaamse provincies, Geen beperkingen op de publieke toegang"],  
     crossOrigin: 'anonymous',
     layer: 'abw',
