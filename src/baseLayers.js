652cd7a319880007ea87cdc9bbc477d730997d28
tragewegen <59048676+tragewegen@users.noreply.github.com>
2023-07-03 15:48:01 +0200

    Update baseLayers.js
    terugzetten waarde 'abw'

--- src/baseLayers.js
+++ src/baseLayers.js
@@ -84,7 +84,7 @@ let abw = new WMTS({
     url: 'https://geo.api.vlaanderen.be/histcart/wmts',
     attributions: ["Vlaamse provincies, Geen beperkingen op de publieke toegang"],  
     crossOrigin: 'anonymous',
-    layer: 'Atlas der Buurtwegen',
+    layer: 'abw',
     matrixSet: 'GoogleMapsVL',
     format: 'image/png',
     projection: webMercator,
